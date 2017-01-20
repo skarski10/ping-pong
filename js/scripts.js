f0c9aa936c35290a068750f01abaa7e55ddd4c48
Kory Andrew Skarbek <Skarski@Korys-MacBook-Pro.local>
2017-01-20 08:27:52 -0800

    add function to count numbers

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+//Busibness logic
+function count (userNumber) {
+  var results = [];
+  var userNumber = 10; //for testing
+  for (i = 1; i <= userNumber; i++) {
+      results.push(i);
+      console.log(results);
+  }
+}
+
+
+
+
+//User logic
