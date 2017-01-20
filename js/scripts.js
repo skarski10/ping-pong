cf71f91fbeeed7d8ff2a6d742ffb34dedc973c09
Kory Andrew Skarbek <Skarski@Korys-MacBook-Pro.local>
2017-01-20 10:59:02 -0800

    add if else statement that will count up to user number replaceing any number divisible by 3 with ping

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,36 @@
 //Busibness logic
+// var results = [];
+// function count (userNumber) {
+//   var numberInput = userNumber;
+//   var userNumberArray = [];
+//   var numberToString = numberInput.toString();
+//   for (n = 1; n <= userNumber; n++) {
+//     for (i = 0; i < n; i += 1) {
+//     userNumberArray.push(+numberToString.charAt(i));
+//     var sum = userNumberArray.reduce(function(a, b) {
+//       return a + b;
+//     });
+//       if (sum % 3 != 0) {
+//         results.push(n);
+//       } else if (sum % 3 == 0) {
+//         results.push('Ping')
+//       }
+//       console.log(results);
+//     }
+//   }
+// }
+
 var results = [];
+
 function count (userNumber) {
-  // var userNumber = 10; //for testing
   for (i = 1; i <= userNumber; i++) {
-          console.log(results);
+    if (i % 3 && i % 5) {
+      results.push(i);
+    } else if (i % 3 == 0) {
+      results.push('Ping');
+    }
   }
 }
-
 
 
 
